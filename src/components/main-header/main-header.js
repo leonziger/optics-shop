0355546fd214fa76400bfc21f4789500da054305
Leonid Zychkov <buldog52@gmail.com>
2020-08-23 12:23:09 +0300

    first commit

---
+++ src/components/main-header/main-header.js
@@ -0,0 +1,21 @@
+import $ from 'jquery';
+import vex from 'vex-js';
+import callback from '../callback/callback';
+
+
+$('.main-header__callback-link').click(function (e) {
+    e.preventDefault();
+    const modal = $(`[data-modal=callback]`);
+
+    if (!modal.length) {
+        return console.error('Modal is not exist!');
+    }
+
+    vex.open({
+        unsafeContent: modal.html(),
+        closeClassName: 'modal__close',
+        afterOpen: function () {
+            callback.mask();
+        }
+    });
+});
